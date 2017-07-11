5814246937cac6868ac8c06e4b68a8ba80504af4
Ilya Seliverstov <ilya@amudo.com>
2017-07-11 11:20:42 +0200

    fix code

--- Example/WSTagsFieldExample/ViewController.swift
+++ Example/WSTagsFieldExample/ViewController.swift
@@ -15,48 +15,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tagsView.addSubview(tagsField)
         
         tagsField.placeholder = "Enter a tag"
         tagsField.backgroundColor = .lightGray
-        tagsField.frame = CGRect(x: 0, y: 44, width: 200, height: 44)
-        tagsView.addSubview(tagsField)
         tagsField.frame = tagsView.bounds
         tagsField.returnKeyType = .next
 
-        // Events
-        tagsField.onDidAddTag = { _ in
-            print("DidAddTag")
-        }
-
-        tagsField.onDidRemoveTag = { _ in
-            print("DidRemoveTag")
-        }
-
-        tagsField.onDidChangeText = { _, text in
-
-        }
-
-        tagsField.onDidBeginEditing = { _ in
-            print("DidBeginEditing")
-        }
-
-        tagsField.onDidEndEditing = { _ in
-            print("DidEndEditing")
-        }
-
-        tagsField.onDidChangeHeightTo = { sender, height in
-            print("HeightTo \(height)")
-        }
-
-        tagsField.onDidSelectTagView = { _, tagView in
-            print("Select \(tagView)")
-        }
-
-        tagsField.onDidUnselectTagView = { _, tagView in
-            print("Unselect \(tagView)")
-        }
+        textFieldEventss()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tagsField.beginEditing()
+    }
+
     override func viewWillLayoutSubviews() {
         tagsField.frame = tagsView.bounds
     }
@@ -68,31 +41,60 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchTest(_ sender: UIButton) {
-        tagsField.padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        tagsField.spaceBetweenTags = 10.0
-        tagsField.font = .systemFont(ofSize: 12.0)
-        tagsField.tintColor = .green
-        tagsField.textColor = .black
-        tagsField.fieldTextColor = .blue
-        tagsField.selectedColor = .black
-        tagsField.selectedTextColor = .red
-        tagsField.delimiter = ","
-        tagsField.returnKeyType = .go
-        print(tagsField.tags)
+//        tagsField.padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//        tagsField.spaceBetweenTags = 10.0
+//        tagsField.font = .systemFont(ofSize: 12.0)
+//        tagsField.tintColor = .green
+//        tagsField.textColor = .black
+//        tagsField.fieldTextColor = .blue
+//        tagsField.selectedColor = .black
+//        tagsField.selectedTextColor = .red
+//        tagsField.delimiter = ","
+//        tagsField.returnKeyType = .go
+//        print(tagsField.tags)
         
-        // Dealloc test
-        let field = WSTagsField()
-        field.addTag("test1")
-        field.addTag("test2")
-        field.addTag("test3")
-        field.addTag("test4")
+//        // Dealloc test
+//        let field = WSTagsField()
+        tagsField.addTag("test1")
+        tagsField.addTag("test2")
+        tagsField.addTag("test3")
+        tagsField.addTag("test4")
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        if tagsField.isEditing == false {
-            tagsField.beginEditing()
-        }
-        super.viewDidAppear(animated)
-    }
+}
 
+extension ViewController {
+    fileprivate func textFieldEventss() {
+        
+        tagsField.onDidAddTag = { _ in
+            print("DidAddTag")
+        }
+        
+        tagsField.onDidRemoveTag = { _ in
+            print("DidRemoveTag")
+        }
+        
+        tagsField.onDidChangeText = { _, text in
+            
+        }
+        
+        tagsField.onDidBeginEditing = { _ in
+            print("DidBeginEditing")
+        }
+        
+        tagsField.onDidEndEditing = { _ in
+            print("DidEndEditing")
+        }
+        
+        tagsField.onDidChangeHeightTo = { sender, height in
+            print("HeightTo \(height)")
+        }
+        
+        tagsField.onDidSelectTagView = { _, tagView in
+            print("Select \(tagView)")
+        }
+        
+        tagsField.onDidUnselectTagView = { _, tagView in
+            print("Unselect \(tagView)")
+        }
+    }
 }
