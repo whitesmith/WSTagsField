9328f71710e3daf2da3bbad7a7881ada4944fc80
James Yorke <james@residentadvisor.net>
2019-02-14 10:38:00 +0000

    Add method to reload input views of textField

--- Source/WSTagsField.swift
+++ Source/WSTagsField.swift
@@ -326,6 +326,10 @@ open class WSTagsField: UIScrollView {
         // So always attempt to resign without checking.
         self.textField.resignFirstResponder()
     }
+    
+    open func reloadTextFieldInputViews() {
+        self.textField.reloadInputViews()
+    }
 
     // MARK: - Adding / Removing Tags
     open func addTags(_ tags: [String]) {
