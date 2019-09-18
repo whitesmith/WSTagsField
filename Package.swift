88c4b83832ed39b661dd607bd2ad5d033b4edbfd
Ricardo Pereira <ricardopereira@whitesmith.co>
2019-09-18 23:05:48 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "WSTagsField",
-            targets: ["WSTagsField"]),
+            targets: ["WSTagsField"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
