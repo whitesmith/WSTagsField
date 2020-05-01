0b2dcdb174e625eec1e4acb807c49df85a67c602
Ricardo Pereira <ricardopereira@whitesmith.co>
2020-05-01 17:14:33 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "WSTagsField",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
